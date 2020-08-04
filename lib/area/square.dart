89137a0e94ac7de466f73f2808da63acac232b21
= <garvitgalgat@gmail.com>
2020-08-04 13:06:28 +0530

    square.dart updated.

--- lib/area/square.dart
+++ lib/area/square.dart
@@ -6,13 +6,96 @@ class square extends StatefulWidget {
 }
 
 class _squareState extends State<square> {
+  double inputByUser = 0.0;
+  TextEditingController myController = new TextEditingController();
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Inputs'),
+        centerTitle: true,
+      ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          //TODO: https://www.omnicalculator.com/math/area
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Card(
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'assets/square.png',
+                      height: 150,
+                      width: 150,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text('square')
+                  ],
+                ),
+                elevation: 0,
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Text('a = ', style: TextStyle(fontSize: 28),),
+                SizedBox(width: 10,),
+                Expanded(
+                  child: TextField(
+                    controller: myController,
+                    keyboardType: TextInputType.numberWithOptions(
+                        signed: false, decimal: true),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RaisedButton(
+                child: Text('Calculate'),
+                onPressed: (){
+                  setState(() {
+//                    if(myController.text == "" ) {
+//                      inputByUser = 0.0;
+//                      print('input null');
+//                    }
+//
+//                    print('my controller text =${myController.text}');
+                    try{
+                      inputByUser = double.parse(myController.text);
+                    }
+                    catch(e){
+                      inputByUser = 0;
+                    }
+
+                  });
+                },
+              ),
+            ],
+          ),
+
+          SizedBox(height: 20,),
+          // Result
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text((inputByUser*inputByUser).toString())
+            ],
+          ),
         ],
       ),
     );
