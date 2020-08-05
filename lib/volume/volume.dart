5ae234256eb2095b2899559ce7cc85140dcb0952
= <garvitgalgat@gmail.com>
2020-08-05 13:21:40 +0530

    added images for volume

--- lib/volume/volume.dart
+++ lib/volume/volume.dart
@@ -6,14 +6,98 @@ class Volume extends StatefulWidget {
 }
 
 class _VolumeState extends State<Volume> {
+
+  Widget customCard(String image) {
+    // image variable stores the location of the image of the object. ex. assets/rectangle.dart
+    String shape = image.substring(image.indexOf('/')+1, image.length-4);
+
+    // using InkWell to make tap able cards
+    return InkWell(
+      onTap: () {
+        print(image);
+        //Navigator.pushNamed(context, '/${shape}');
+      },
+      child: Card(
+        child: Column(
+          children: [
+            Image.asset(image, height: 150, width: 150,),
+            SizedBox(height: 10,),
+            Text(shape)
+          ],
+        ),
+        elevation: 0,
+
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
-        //TODO: Volume not implemented
-        child: Center(child: Text('Not Implemented Yet')),
-      )
-      ,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(2, 10, 2, 10),
+          // ListView to enable vertical scrolling
+          child: ListView(
+            children: [
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      customCard('assets/box.png'),
+                      customCard('assets/capsule.png'),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      customCard('assets/cone.png'),
+                      customCard('assets/cube.png'),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      customCard('assets/cylinder.png'),
+                      customCard('assets/cylinder-hollow.png'),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      customCard('assets/ellipsoid.png'),
+                      customCard('assets/frustum.png'),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      customCard('assets/hemisphere.png'),
+                      customCard('assets/pyramid.png'),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      customCard('assets/pyramid-truncated.png'),
+                      customCard('assets/sphere.png'),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      customCard('assets/spherical-cap.png'),
+                      customCard('assets/triangular-prism.png'),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+
     );
   }
 }
